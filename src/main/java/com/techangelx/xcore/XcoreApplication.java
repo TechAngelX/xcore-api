fbca3e8448b0836a97a92114931a4728caf91b83
Ricki Angel <angelricki@outlook.com>
2026-02-11 21:17:19 +0000

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/techangelx/xcore/XcoreApplication.java
@@ -0,0 +1,13 @@
+package com.techangelx.xcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techangelx/xcore/XcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techangelx.xcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
